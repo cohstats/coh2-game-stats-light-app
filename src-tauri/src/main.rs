d0050ac52095a733670de1979fb363cf41a4cf2b
Johannes Merkt <mailjohannesmerkt@gmail.com>
2022-12-19 19:42:25 +0100

    Add shadow to windows

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,12 +5,19 @@
 
 use tauri_plugin_fs_watch::Watcher;
 use std::path::Path;
+use tauri::Manager;
+use window_shadows::set_shadow;
 mod parse_log_file;
 
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![get_default_log_file_path, check_log_file_exists, parse_log_file::parse_log_file_reverse])
         .plugin(Watcher::default())
+        .setup(|app| {
+            let window = app.get_window("main").unwrap();
+            set_shadow(&window, true).expect("Unsupported platform!");
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
